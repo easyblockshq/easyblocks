d47cfbc212e21cf62e47cb5da1ad3f26d6c2d884
Andrzej Dabrowski <a.r.dabrowski@gmail.com>
2023-10-16 16:31:53 +0200

    More templates

---
+++ apps/quick-demo/src/app/easyblocks/templates/nooma/BasicCard2/NoomaBasicCard2.ts
@@ -0,0 +1,9 @@
+import { Template } from "@easyblocks/core";
+import entry from "./NoomaBasicCard2Entry.json";
+
+export const NoomaBasicCard2: Template = {
+  id: "BasicCard2",
+  config: entry,
+  thumbnail:
+    "https://images.ctfassets.net/blh4anz05qu1/4TKdp7GmUp7gOYPVKDR7Zj/c96e32d829798cf882b43199dc829c36/Screenshot_2023-10-16_at_16.13.57.png",
+};
---
+++ apps/quick-demo/src/app/easyblocks/templates/nooma/BasicCard3/NoomaBasicCard3.ts
@@ -0,0 +1,9 @@
+import { Template } from "@easyblocks/core";
+import entry from "./NoomaBasicCard3Entry.json";
+
+export const NoomaBasicCard3: Template = {
+  id: "BasicCard3",
+  config: entry,
+  thumbnail:
+    "https://images.ctfassets.net/blh4anz05qu1/KpMpJxM64cBObZinCU5Of/61c787895bb51c3fa006a59cc12838ab/Screenshot_2023-10-16_at_16.17.47.png",
+};
